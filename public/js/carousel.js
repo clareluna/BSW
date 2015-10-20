2c424ac9aa4643ec2a10f38759b90b0b235087ac
Jose Tello <josectello@gmail.com>
2015-10-20 16:55:11 -0700

    carousel transitions properly, footer and content require proper display below carousel

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,27 +1,21 @@
 // $(function() {
-
-// 	var slides = ['img/slide1.jpg', 'img/slide2.jpg', 'img/slide3.jpg'];
-
-// 	function changeSlide(arr) {
-// 		$('#slideshow').attr('src', arr[0]).delay(2500).fadeOut(1000);
-// 		var i = 1;
-// 		setInterval(function() {
-// 			$('#slideshow').fadeIn(1000).attr('src', arr[i]).delay(1500).fadeOut(1000);
-// 			i++;
-// 			if (i >= arr.length) {
-// 				i = 0;
-// 			}
-// 		}, 3500);
-// 	}
-// 	changeSlide(slides);
+//   function changeSlide(el) {
+//     $(el).fadeIn(2500, function() {
+//       $(this).fadeOut(2500, function() {
+//         changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)
+//       });
+//     });
+//   }
+//   changeSlide($(".slideshow:first"));
 // });
+
 
 $(function() {
   function changeSlide(el) {
     $(el).fadeIn(2500, function() {
-      $(this).fadeOut(2500, function() {
-        changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)
-      });
+      $(this).delay(3000).fadeOut({duration:2500, start:function() {
+				changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)}
+			});
     });
   }
   changeSlide($(".slideshow:first"));
