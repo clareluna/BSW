2ebd8378a9917f00c86df6203512e897ef52e167
Jose Tello <josectello@gmail.com>
2015-10-26 13:59:12 -0700

    absolute positioning not working

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,19 +1,7 @@
-// $(function() {
-//   function changeSlide(el) {
-//     $(el).fadeIn(2500, function() {
-//       $(this).fadeOut(2500, function() {
-//         changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)
-//       });
-//     });
-//   }
-//   changeSlide($(".slideshow:first"));
-// });
-
-
 $(function() {
   function changeSlide(el) {
     $(el).fadeIn(2500, function() {
-      $(this).delay(3000).fadeOut({duration:2500, start:function() {
+      $(this).delay(3000).fadeOut({duration: 2500, start: function() {
 				changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)}
 			});
     });
