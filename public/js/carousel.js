2458d047191a3c1647ab763b9428cb76a3c39bd7
Jose Tello <josectello@gmail.com>
2015-10-27 20:44:29 -0700

    move code into partials, fix gulpfile

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,10 +1,10 @@
-$(function() {
-  function changeSlide(el) {
-    $(el).fadeIn(2500, function() {
-      $(this).delay(3000).fadeOut({duration: 2500, start: function() {
-				changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)}
-			});
-    });
-  }
-  changeSlide($(".slideshow:first"));
-});
+// $(function() {
+//   function changeSlide(el) {
+//     $(el).fadeIn(2500, function() {
+//       $(this).delay(3000).fadeOut({duration: 2500, start: function() {
+// 				changeSlide(this.nextElementSibling || this.parentElement.firstElementChild)}
+// 			});
+//     });
+//   }
+//   changeSlide($(".slideshow:first"));
+// });
