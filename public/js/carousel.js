741db4016c56eebf0fb555133d9ad062510a6758
Jose Tello <josectello@gmail.com>
2015-11-04 19:33:19 -0800

    static page with correct positioning

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -8,3 +8,18 @@
 //   }
 //   changeSlide($(".slideshow:first"));
 // });
+
+$(function() {
+	function slideDisplay() {
+		var imgs = ['img/slide1.jpg', 'img/slide2.jpg', 'img/slide3.jpg'];
+		// var src = $('.slideshow').attr('src');
+		
+		setInterval(function() {
+			for (var i = 0; i < imgs.length; i++) {
+				$('#slideshow').attr('src', imgs[i]);
+			}
+		}, 3000);
+
+	}
+	slideDisplay();
+});
