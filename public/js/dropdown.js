1f81f03e1c09f162c6ef603e2075d23d84453531
Jose Tello <josectello@gmail.com>
2015-11-04 20:56:15 -0800

    hide ul links on mobile breakpoint, expand content height

---
+++ public/js/dropdown.js
@@ -0,0 +1,10 @@
+// $(function() {
+// 	function checkPosition() {
+//     if (window.matchMedia('(min-width: 715px)').matches) {
+//       console.log('hello world');
+//     } else {
+//       $('nav').hide();
+//     }
+// 	}
+// 	checkPosition();
+// });
